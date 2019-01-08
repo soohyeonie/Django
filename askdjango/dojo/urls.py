a70ec77b72a5c44bb060bde5180921c997468839
soohyeonie <soohyeonie@naver.com>
2019-01-08 22:45:05 +0900

    함수 기반 뷰(FBV)의 4가지 패턴

--- askdjango/dojo/urls.py
+++ askdjango/dojo/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     re_path(r'^sum/(?P<numbers>[\d/]+)/$',views.mysum),
-    re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello)
+    re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
+    re_path(r'^list1/$',views.post_list1),
+    re_path(r'^list2/$',views.post_list2),
+    re_path(r'^list3/$',views.post_list3),
+    re_path(r'^excel/$',views.excel_download),
 ]
--- askdjango/dojo/views.py
+++ askdjango/dojo/views.py
@@ -1,6 +1,8 @@
 #dojo/views.py
+import os
+from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 
 def mysum(request, numbers):
     # result=sum(map(int,numbers.split("/")));
@@ -9,4 +11,30 @@ def mysum(request, numbers):
 
 def hello(request, name ,age):
     return HttpResponse('안녕하세요. {}. {}살 이시네요.'.format(name,age))
-# Create your views here.
+
+def post_list1(request):
+    name = '공유';
+    return HttpResponse(('''
+    <h1>AskDjango</h1>
+    <p>{name}</p>
+    <p>여러분의 파이썬&장고 페이스메이커가 되어드리겠습니다.</p>
+    ''').format(name=name))
+
+def post_list2(request):
+    name='공유';
+    return render(request,'dojo/post_list.html',{'name':name})
+    
+def post_list3(request):
+    return JsonResponse({
+        'message' : '안녕, 파이썬&장고',
+        'items' : ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+    }, json_dumps_params={'ensure_ascii':False})
+
+def excel_download(request):
+    # filepath = r'c:\dev\Django\askdjango\ie_data.xls'
+    filepath = os.path.join(settings.BASE_DIR,'ie_data.xls')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
